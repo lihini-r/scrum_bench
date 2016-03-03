b3dc66638fd71f0a1f3f40d13380364e65f2aee4
hashi-n <hashi93rox@gmail.com>
2016-03-03 06:08:30 +0530

    Added database/migrations/2016_02_21_080514_add_pm_to_projects.php

---
+++ database/migrations/2016_02_21_080514_add_pm_to_projects.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPmToProjects extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('projects', function(Blueprint $table)
+		{
+			$table->string('pm')->default('no');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn('pm');
+		});
+	}
+
+}
