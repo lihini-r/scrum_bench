a73211c56cfdd3d8bf0a84e93e02c0f02471053c
hashi-n <hashi93rox@gmail.com>
2016-03-03 06:23:31 +0530

    Modified database/seeds/DatabaseSeeder.php to support new db structure

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,17 +18,65 @@ class DatabaseSeeder extends Seeder {
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('admin123'),
+			'designation' => 'Admin',
         ]);
 		DB::table('users')->insert([
             'name' => 'dev',
             'email' => 'dev@gmail.com',
             'password' => bcrypt('dev123'),
+			'designation' => 'Developer',
         ]);
 		DB::table('users')->insert([
             'name' => 'pm',
             'email' => 'pm@gmail.com',
             'password' => bcrypt('pm123'),
+			'designation' => 'Developer',
         ]);
+
+		DB::table('users')->insert([
+			'name' => 'parmer',
+			'email' => 'sajjf@gmail.com',
+			'password' => bcrypt('paramer123'),
+
+			'designation' => 'Project Manager',
+		]);
+
+		DB::table('users')->insert([
+			'name' => 'adam',
+			'email' => 'addam@gmail.com',
+			'password' => bcrypt('adam123'),
+			'designation' => 'Project Manager',
+
+		]);
+
+		DB::table('users')->insert([
+			'name' => 'Amal',
+			'email' => 'kamal@gmail.com',
+			'password' => bcrypt('kamal123'),
+			'designation' => 'Account Head',
+
+		]);
+
+
+		DB::table('users')->insert([
+			'name' => 'Sunil',
+			'email' => 'amal@gmail.com',
+			'password' => bcrypt('amal123'),
+			'designation' => 'Account Head',
+
+		]);
+		DB::table('users')->insert([
+			'name' => 'kusal',
+			'email' => 'kusal@gmail.com',
+			'password' => bcrypt('kusal123'),
+			'designation' => 'Developer',
+
+		]);
+
+
+
+
+
 	}
 
 }
