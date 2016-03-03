49d4de18574c7855420b813ed92f168479d92263
Dhanushkad6 <danushkad2008@gmail.com>
2016-03-03 23:08:44 +0530

    Formatted source relevant to Account Head Dashboard section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,11 +9,10 @@ $accounts = DB::table('accounts')->get();
 ?>
 
 @section('content')
-<br/>
-<div style="width:80%;padding:5px 5px 15px 80px;">
-<div class="panel panel-success" >
-	<div class="panel-heading">Home</div>
-
+	<br/>
+	<div style="width:80%;padding:5px 5px 15px 80px;">
+		<div class="panel panel-success" >
+			<div class="panel-heading">Home</div>
 			<div class="panel-body">
 
 				<div class="panel-body">
@@ -55,50 +54,42 @@ $accounts = DB::table('accounts')->get();
 						@endforeach
 						</tbody>
 					</table>
-
 				</div>
 			</div>
-
 		</div>
 		
 		<h1>Account Head Dashboard</h1>
 		<br>
 
 		<?php $c= count(DB::table("projects"));?>
-		@foreach($projects as $key => $project)
+			@foreach($projects as $key => $project)
 			<?php $c=$c+1?>
 
 			<div class="container" style="position: relative;left: auto">
 				<div class="col-md-12">
-				<div class="row">
-				<div class="col-lg-3 col-xs-6">
-					<!-- small box -->
-<table>		<tr>
-
-		<div class="small-box bg-aqua" >
-						<div class="inner">
-							<h4><b>{{ $project->ProjectName }}</b></h4>
-							<p>{{ $project->ProjectID }}</p>
+					<div class="row">
+						<div class="col-lg-3 col-xs-6">
+							<!-- small box -->
+							<table>
+								<tr>
+									<div class="small-box bg-aqua" >
+										<div class="inner">
+											<h4><b>{{ $project->ProjectName }}</b></h4>
+											<p>{{ $project->ProjectID }}</p>
+										</div>
+										<a href="{{ URL::to('projects/' . $project->ProjectID) }}" class="small-box-footer">
+												More info <i class="fa fa-arrow-circle-right"></i>
+										</a>
+									</div>
+								</tr>
+							</table>
 						</div>
-
-						<a href="{{ URL::to('projects/' . $project->ProjectID) }}" class="small-box-footer">
-							More info <i class="fa fa-arrow-circle-right"></i>
-						</a>
 					</div>
-	</tr>
-</table>
-
 				</div>
-		</div>
-					</div>
-
 				<!-- we will also add show, edit, and delete buttons -->
 			</div>
-
-		@endforeach
-		<?php echo $c?>
-		
-	</div>
+			@endforeach
+		<?php echo $c?>		
 	</div>
 	{{--<img src="{{ URL::asset('dist/img/project-team1.png') }}" alt="Team Image">--}}
 @endsection
