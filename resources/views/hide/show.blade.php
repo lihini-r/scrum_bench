4dd834f7984b00f8687a0605794e82d583402ecd
hashi-n <hashi93rox@gmail.com>
2016-04-26 13:42:50 +0530

    modified team view in pm dashboard

--- resources/views/hide/show.blade.php
+++ resources/views/hide/show.blade.php
@@ -1,3 +1,4 @@
+<!--view team details in pm dashboard--->
 
 @extends('app')
 <?php
@@ -85,8 +86,118 @@ use \App\User;
                     <br>
                 </div>
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div class="row">
+
+            <!--view developer profiles-->
+
+
+                    <?php
+                    $devs=DB::table('dev_team')->where('team_id',$team->team_id)->get();
+                    $users = User::where('designation','Developer')->get();
+
+
+                    $user_id_name = array();
+                    foreach($users as $user){
+                        $user_id_name[$user->id] = $user->name;
+
+
+                    }
+                    ?>
+
+                    <?php
+
+
+
+
+                    foreach ($devs as $dev) {
+
+
+
+                        $profile=DB::table('profiles')->where('id',$dev->user_id)->get();
+
+                        foreach($profile as $pro)
+                        {
+                          echo " <div class='col-md-4'>";
+
+
+                            echo "<div class='box box-widget widget-user-2'>
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class='widget-user-header bg-green-active'>
+            <div class='widget-user-image'>
+                 <img class='img-circle' src='../../../dist/img/pm.png'>
+            </div><!-- /.widget-user-image -->";
+
+
+                            $name=$user_id_name[$dev->user_id];
+                            echo " <h3 class='widget-user-username'>$name</h3>";
+                            //echo $user_id_name[$dev->user_id];
+
+                            echo "<br>";
+                            echo " <h5 class='widget-user-desc'>$pro->about</h5>";
+
+
+                            echo  "<div class='box-footer no-padding'>
+                <ul class='nav nav-stacked'>
+                    <li><a href='#'>Professional Qualifications <span class='pull-right badge bg-red'>$pro->prof_qual</span></a></li>
+                    <li><a href='#'>Academic Qualifications <span class='pull-right badge bg-aqua'>$pro->acad_qual</span></a></li>
+                    <li><a href='#'>Technology <span class='pull-right badge bg-yellow-active'>$pro->techno</span></a></li>
+
+                </ul>
+                                    </div>";
+
+                            echo " </div>";
+                            echo " </div>";
+                            echo " </div>";
+
+
+                        }
+
+
+
+
+
+
+
+                    }
+
+                    ?>
+
+                </div>
         </div>
-    </div>
+
+            </div>
+
+
+
+        <!-- /.widget-user -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
+
 
 
